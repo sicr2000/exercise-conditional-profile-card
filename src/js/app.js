5fb943b73bacb882b8c8f14bd7c35a055f15a5e9
bynx <138079781+sicr2000@users.noreply.github.com>
2023-08-10 05:09:31 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,8 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          <h1> ${variables.name == null ? "Your Name" : variables.name} ${
+    variables.lastname == null ? "Your Lastname" : variables.lastname
+  }</h1>          <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
